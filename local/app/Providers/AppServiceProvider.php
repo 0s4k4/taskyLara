de0d1d74c0575f4087002cd3e1bff13b7168165b
Your Name <you@example.com>
2021-05-11 10:28:24 -0500

    se agrega configuracion para https

--- local/app/Providers/AppServiceProvider.php
+++ local/app/Providers/AppServiceProvider.php
@@ -15,7 +15,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-       Schema::defaultStringLength(191);
+       if(config(key: 'app.env') == 'production')
+       {
+           URL::forceScheme('https');
+       }
     }
 
     /**
